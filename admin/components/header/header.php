3f5e1ab9fd224e976ded977bdf2266235e10dda8
Duong <c3tanlaphanoi@gmail.com>
2023-12-15 15:06:31 +0700

    update db

--- admin/components/header/header.php
+++ admin/components/header/header.php
@@ -5,7 +5,9 @@
     <h3>Trường trung học phổ thông 123</h3>
     <div class="user-infor">
         <p><?php echo $_SESSION["user"]["name"] ?></p>
-        <div class="avata"></div>
+        <div class="avata">
+            <img src="./avatas/<?php echo $_SESSION['user']['avata'] ?>" alt="">
+        </div>
         <?php include('components/dropdownHeader/dropdownHeader.php')?>
               
     </div>
